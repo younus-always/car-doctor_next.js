67756b4902b6dbd2b02d83c5a2790233bd146990
younus-always <younusofficial443@gmail.com>
2025-05-23 16:05:48 +0600

    service section ui design

--- src/app/components/SectionHeader.jsx
+++ src/app/components/SectionHeader.jsx
@@ -2,8 +2,8 @@
 const SectionHeader = ({ title, subTitle }) => {
       return (
             <div className="max-w-xl mx-auto text-center py-8">
-                  <span className="text-2xl font-bold text-orange-500">{subTitle}</span>
-                  <h2 className="text-5xl font-bold py-5">{title}</h2>
+                  <span className="text-xl xl:text-2xl font-bold text-orange-500">{subTitle}</span>
+                  <h2 className="text-4xl xl:text-5xl font-bold py-5">{title}</h2>
                   <p className="capitalize font-semibold text-gray-500">
                         the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
                   </p>
--- src/app/page.js
+++ src/app/page.js
@@ -8,26 +8,11 @@ export default function Home() {
   return (
     <div>
       <h2 className="text-3xl font-semibold text-center py-5">Home page</h2>
-      <section className="py-8">
-        <ServiceSection />
-        <div></div>
-      </section>
-      <section className="py-8">
-        <ProductSection />
-        <div></div>
-      </section>
-      <section className="py-8">
-        <TeamSection />
-        <div></div>
-      </section>
-      <section className="py-8">
-        <ChooseUsSection />
-        <div></div>
-      </section>
-      <section className="py-8">
-        <TestimonialSection />
-        <div></div>
-      </section>
+      <ServiceSection />
+      <ProductSection />
+      <TeamSection />
+      <ChooseUsSection />
+      <TestimonialSection />
     </div>
   );
 }
