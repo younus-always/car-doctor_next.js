2cbd9ba780d7b0d5e54c2adbff2b732ca301017c
younus-always <younusofficial443@gmail.com>
2025-06-10 20:32:36 +0600

    middleware function to get token from getToken function and protect some routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,20 @@
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server"
 
 export const middleware = async (req) => {
-      
-      return NextResponse.next()
+      console.log("From middleware ", req.nextUrl.pathname)
+      const token = await getToken({ req })
+      if (token) {
+            return NextResponse.next()
+      } else {
+            return NextResponse.redirect(new URL('/login', req.URL))
+      }
+
+};
+export const config = {
+      matcher: [
+            '/my-booking',
+            '/my-booking/:path*',
+            '/checkout/:path*'
+      ]
 }
