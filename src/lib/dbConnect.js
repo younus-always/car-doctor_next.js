47fd998bd0b9cd91a0fda025c80f4fe0d3256526
younus-always <younusofficial443@gmail.com>
2025-06-11 22:11:41 +0600

    deploying next-js-car-doctor

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -7,7 +7,7 @@ export const collectionNameObj = {
 };
 
 export default function dbConnect(collectionName) {
-      const uri = process.env.MONGODB_URI
+      const uri = process.env.NEXT_PUBLIC_MONGODB_URI
       const client = new MongoClient(uri, {
             serverApi: {
                   version: ServerApiVersion.v1,
